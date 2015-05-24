b8dcdc623ca3b38164344448723206f772ac16a8
Steve Kinney <hello@stevekinney.net>
2015-05-23 20:51:02 -0600

    Basic functionality

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-module.exports = function () {
+module.exports = function (values, minimum, maximum) {
+
+  const range = minimum - maximum - 1;
+  const segmentSize = values.length / range;
+
+  return function (value) {
+    if (value < minimum || value > maximum) {
+      throw new Error('Value, ' + value + ', is out of range: ' + minimum + ' to ' + maximum + '.');
+    }
+
+    const index = Math.floor((value - minimum) * -segmentSize + minimum) - 1;
+    return values[index];
+  };
 
 };
--- test/scale-buckets-test.js
+++ test/scale-buckets-test.js
@@ -3,8 +3,51 @@ const scaleBuckets = require('../index.js');
 
 describe('scaleBuckets', function () {
 
-  it('should return a function', function () {
+  before(function () {
+    this.values = ['a', 'b', 'c', 'd', 'e'];
+    this.scale = scaleBuckets(this.values, 1, 10);
+  });
+
+  it('should be a function', function () {
     assert.equal(typeof scaleBuckets, 'function');
+  });
+
+  it('should return a function', function () {
+    assert.equal(typeof this.scale, 'function');
+  });
+
+  it('should return a value from the array supplied', function () {
+    assert.notStrictEqual(this.values.indexOf(this.scale(5), -1));
+  });
+
+  it('should return the first value if its in the first bucket', function () {
+    assert.strictEqual(this.scale(1), 'a');
+    assert.strictEqual(this.scale(2), 'a');
+  });
+
+  it('should return the second value if its in the second bucket', function () {
+    assert.strictEqual(this.scale(3), 'b');
+    assert.strictEqual(this.scale(4), 'b');
+  });
+
+  it('should return the third value if its in the third bucket', function () {
+    assert.strictEqual(this.scale(5), 'c');
+    assert.strictEqual(this.scale(6), 'c');
+  });
+
+  it('should return the fourth value if its in the fourth bucket', function () {
+    assert.strictEqual(this.scale(7), 'd');
+    assert.strictEqual(this.scale(8), 'd');
+  });
+
+  it('should return the fifth value if its in the fifth bucket', function () {
+    assert.strictEqual(this.scale(9), 'e');
+    assert.strictEqual(this.scale(10), 'e');
+  });
+
+  it('should throw an error if the input is out of range', function () {
+    assert.throws(function () { this.scale(0)}.bind(this), /range/);
+    assert.throws(function () { this.scale(11)}.bind(this), /range/);
   });
 
 });
